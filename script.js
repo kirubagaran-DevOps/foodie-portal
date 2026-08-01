f7b37778602fb7cb7bf3540db7948a632007913a
kiruba <mkirubagaran@griddynamics.com>
2026-08-01 10:15:59 +0530

    add a small change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const restaurants = [
   { id: 1, name: 'The Pizza Project', cuisine: 'Italian', dish: 'Truffle mushroom pizza', price: 349, time: '25–30 min', rating: '4.7', emoji: '🍕', color: '#eaa86e' },
   { id: 2, name: 'Bowl & Bloom', cuisine: 'Healthy', dish: 'Avocado harvest bowl', price: 289, time: '20–25 min', rating: '4.6', emoji: '🥗', color: '#9ecb96' },
-  { id: 3, name: 'Namma Biryani', cuisine: 'Indian', dish: 'Hyderabadi chicken biryani', price: 329, time: '30–35 min', rating: '4.8', emoji: '🍛', color: '#e5bd61' },
+  { id: 3, name: 'Nei soru', cuisine: 'Indian', dish: 'Hyderabadi chicken biryani', price: 329, time: '30–35 min', rating: '4.8', emoji: '🍛', color: '#e5bd61' },
   { id: 4, name: 'Bao House', cuisine: 'Asian', dish: 'Crispy tofu bao', price: 259, time: '25–30 min', rating: '4.5', emoji: '🥟', color: '#e7a3a3' },
   { id: 5, name: 'Dosa District', cuisine: 'Indian', dish: 'Masala dosa', price: 179, time: '15–20 min', rating: '4.7', emoji: '🥞', color: '#d9b778' },
   { id: 6, name: 'Pasta Parade', cuisine: 'Italian', dish: 'Creamy pesto penne', price: 319, time: '25–30 min', rating: '4.4', emoji: '🍝', color: '#c9a08c' }
